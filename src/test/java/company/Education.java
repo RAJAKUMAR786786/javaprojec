4717ca7035b97f3117247489121be3ff5f843570
RAJAKUMAR786786 <snowart332@gmail.com>
2022-01-27 10:03:13 -1200

    commit message

---
+++ src/test/java/company/Education.java
@@ -0,0 +1,12 @@
+package company;
+
+public class Education {
+public void tamil(){
+	System.out.println("I know tamil");
+}
+	public static void main(String[] args){
+		Education ed=new Education();
+		ed.tamil();
+	}
+}
+
